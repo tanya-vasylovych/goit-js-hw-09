f6309ba6099fed68ecab1c84604f310b3df60a34
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-10 15:48:19 +0300

    form-html/css

---
+++ src/js/2-form.js
@@ -0,0 +1,8 @@
+const formData = { email: "", message: "" };
+const feedbackForm = document.querySelector('.feedback-form');
+const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+feedbackForm.addEventListener('input', (event) => {
+    const target = event.target;
+})
+
