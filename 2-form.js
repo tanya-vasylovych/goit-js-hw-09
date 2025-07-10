6a599155e593fbc97ba5db872d3d770c83033717
tanya-vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-10 12:48:34 +0000

    Deploying to gh-pages from @ tanya-vasylovych/goit-js-hw-09@f6309ba6099fed68ecab1c84604f310b3df60a34 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=document.querySelector(".feedback-form");JSON.parse(localStorage.getItem("feedback-form-state"));t.addEventListener("input",e=>{e.target});
+//# sourceMappingURL=2-form.js.map
