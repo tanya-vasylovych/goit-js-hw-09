da57488b62ace3040c72dd60da17aaa2de3e55b7
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-10 16:59:19 +0300

    2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,37 @@
 const formData = { email: "", message: "" };
-const feedbackForm = document.querySelector('.feedback-form');
+const form = document.querySelector('form');
+
 const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (savedData) {
+  formData.email = savedData.email || "";
+  formData.message = savedData.message || "";
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
 
-feedbackForm.addEventListener('input', (event) => {
-    const target = event.target;
-})
+const onFormInput = event => {
+    if (event.target.name === 'email' || event.target.name === 'message') {
+        formData[event.target.name] = event.target.value;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    }
+};
 
+
+const onFormSubmit = event => {
+    event.preventDefault();
+
+    if (!formData.email.trim() || !formData.message.trim()) {
+        alert("Fill please all fields");
+        return;
+    }
+  
+    console.log(formData);
+  
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+};
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
